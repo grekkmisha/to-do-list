6b8726127a42486ccfaf110cb2873aa05f5f1e14
Misha Grekk <grekkmisha@gmail.com>
2022-11-02 21:20:49 +0300

    13 урок

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const todoControl = document.querySelector('.todo-control'),
+let todoControl = document.querySelector('.todo-control'),
     headerInput = document.querySelector('.header-input'),
     todoList = document.querySelector('.todo-list'),
     todoCompleted = document.querySelector('.todo-completed'),
@@ -22,21 +22,48 @@ const todoControl = document.querySelector('.todo-control'),
                 todoList.append(li);
             }
 
+            li.querySelector('.todo-remove').addEventListener('click', function () {
+                li.remove();
+                for (let i = 0; i < todoData.length; i++) {
+                    if (todoData[i] === item) {
+                        todoData.splice(i, 1);
+                    }
+                }
+                localStorage.removeItem('toDo', JSON.stringify(todoData));
+            });
+
             li.querySelector('.todo-complete').addEventListener('click', function () {
                 item.completed = !item.completed;
+                localStorage.setItem('toDo', JSON.stringify(todoData));
                 render();
             });
         });
+    },
+
+    validation = function () {
+        if (headerInput.value === '') {
+            return false;
+        } else {
+            return true;
+        }
     };
+
+if (localStorage.getItem('toDo')) {
+    todoData = JSON.parse(localStorage.getItem('toDo'));
+    render();
+}
 
 todoControl.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    const newToDo = {
-        text: headerInput.value,
-        completed: false
-    };
-    todoData.push(newToDo);
+    if (validation() === true) {
+        const newToDo = {
+            text: headerInput.value,
+            completed: false,
+        };
+        todoData.push(newToDo);
+        localStorage.setItem('toDo', JSON.stringify(todoData));
+        render();
+    }
     headerInput.value = '';
-    render();
 });
